83485dfe14e54c57dc26ce1f82082891b847fe73
ramishahq <hoque2@usf.edu>
2024-11-03 16:05:50 -0500

    Use finally to clean up after the fetch

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 //Task 2 
 const container = document.getElementById('container');
-const loadingMessage = document.getElementById('loading');
+const processingMessage = document.getElementById('processing');
 
 async function fetchTickets() {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts');
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('No response');
         }
         const tickets = await response.json();
         if (tickets.length === 0) {
-            throw new Error('No tickets found');
+            throw new Error('No tickets are found');
         }
         displayTickets(tickets);
     } catch (error) {
         container.innerHTML = `<p>Error: ${error.message}</p>`;
+        
+        // Task 4 Use finally to Ensure Cleanup
+    }finally {
+        processingMessage.style.display = 'ensurrig cleanup';
     }
+}
+
 
     
-}
+
 
 // Task 3 
 function displayTickets(tickets) {
@@ -35,3 +41,5 @@ function displayTickets(tickets) {
 }
 // Calling the function to fetch ticket details
 fetchTickets();
+
+
